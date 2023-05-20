8b88fdc599b3e7d500dde593679ee52580390a13
Christian Berkman <christianberkman@gmail.com>
2023-05-20 17:25:12 +0300

    Service Worker Path and Scope

--- index.js
+++ index.js
@@ -55,9 +55,9 @@ const registerServiceWorker = async () => {
     if ("serviceWorker" in navigator) {
       try {
         const registration = await navigator.serviceWorker.register(
-          "/serviceworker.js", 
+          "/ug-timetable/serviceworker.js?v=20230520", 
           {
-            scope: "/"
+            scope: "/ug-timetable"
           }
         );
 
